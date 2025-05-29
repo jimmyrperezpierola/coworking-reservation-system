12dcdf361b7336899aff0510f71906f6587797cb
elproffe <mmonterde.app@gmail.com>
2025-05-28 20:43:23 -0400

    Docker

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000', credentials: true }));
 
 // Importar rutas
 const authRoutes = require('./routes/authRoutes');

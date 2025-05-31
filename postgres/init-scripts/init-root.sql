584ce2eaa8c038ac39d3f9f7ebd2b8fb7c20971f
elproffe <mmonterde.app@gmail.com>
2025-05-31 18:16:07 -0400

    User Root - Correction

--- postgres/init-scripts/init-root.sql
+++ postgres/init-scripts/init-root.sql
@@ -5,7 +5,7 @@ BEGIN
   IF NOT EXISTS (
     SELECT FROM pg_catalog.pg_roles WHERE rolname = 'root'
   ) THEN
-    CREATE ROLE root WITH LOGIN SUPERUSER PASSWORD 'StrongRootPassword123!';
+    CREATE ROLE root WITH LOGIN SUPERUSER PASSWORD 'root_password';
     RAISE NOTICE '🔐 Usuario root creado.';
   ELSE
     RAISE NOTICE 'ℹ️ Usuario root ya existe, se omite.';
